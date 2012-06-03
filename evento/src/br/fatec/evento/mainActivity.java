2147905835d430f39f84a4ecd4094a11ae2186a9
shikamarujunior <shikamarujunior@ebf3cafe-77c0-995b-8895-a9585d18da41>
2012-06-03 01:32:48 +0000

    Inclusão do menu para acesso as configurações.

--- evento/src/br/fatec/evento/mainActivity.java
+++ evento/src/br/fatec/evento/mainActivity.java
@@ -3,6 +3,7 @@ package br.fatec.evento;
 import android.app.Activity; 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,6 +11,8 @@ import android.widget.Button;
 
 public class mainActivity extends Activity implements OnClickListener {
 	/** Called when the activity is first created. */
+	public final int CONFIG = 0;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,4 +45,25 @@ public class mainActivity extends Activity implements OnClickListener {
     		break; 
     	}
     }
+    
+    @Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		// Adiciona opção ABOUT AO MENU
+		menu.add(0, CONFIG, 0, "Configurações");
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		Intent it;
+		switch (item.getItemId()) {
+		case CONFIG:
+			it = new Intent("EVENTO_CONFIG");
+			startActivity(it);
+			return true;
+		default: 
+			return true;
+		}
+	}
 }
