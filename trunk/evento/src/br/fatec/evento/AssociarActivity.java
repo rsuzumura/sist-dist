6908341db8f8aeb9ba23f8ab803f0d9774d549c5
shikamarujunior <shikamarujunior@ebf3cafe-77c0-995b-8895-a9585d18da41>
2012-06-12 23:42:07 +0000

    Label com o titulo do evento na associação de convidados

--- trunk/evento/src/br/fatec/evento/AssociarActivity.java
+++ trunk/evento/src/br/fatec/evento/AssociarActivity.java
@@ -36,7 +36,11 @@ public class AssociarActivity extends ListActivity implements OnClickListener {
         this.setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_checked, items));
         
         TextView lblNomeEvento = (TextView) findViewById(R.id.lblNomeEvento);
-        lblNomeEvento.setText(nomeEvento);
+        
+        if (nomeEvento.equals("")) 
+        	lblNomeEvento.setText(R.string.btnAssociar);
+        else
+        	lblNomeEvento.setText(nomeEvento);
         
         ImageView btnConfirmar = (ImageView) findViewById(R.id.btnConfirmar);
         btnConfirmar.setOnClickListener(this);
