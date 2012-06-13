2a1a46eda49bfac23d103a0709cc5560ef106973
shikamarujunior <shikamarujunior@ebf3cafe-77c0-995b-8895-a9585d18da41>
2012-06-13 04:10:32 +0000

    finalização da estrutura básica da tela de recepção dos convidados

--- evento/src/br/fatec/evento/RecepcaoActivity.java
+++ evento/src/br/fatec/evento/RecepcaoActivity.java
@@ -1,24 +1,78 @@
 package br.fatec.evento;
 
-import android.app.Activity; 
+import br.fatec.evento.classes.Convidado;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
-public class RecepcaoActivity extends Activity implements OnClickListener {
+public class RecepcaoActivity extends ListActivity implements OnClickListener {
 	/** Called when the activity is first created. */
+	
+	String[] items = new String[] { "Convidado 1","Convidado 2","Convidado 3","Convidado 4", "Convidado 5", "Convidado 6"}; 
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recepcao);
         
+        TextView txtTitulo = (TextView) findViewById(R.id.txtTitulo);
+        txtTitulo.setText(R.string.btnRecepcao);
+        
+        ImageView btnInsert = (ImageView) findViewById(R.id.btnInsert);
+        btnInsert.setOnClickListener(this);
+        
+        Button btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
+        btnConfirmar.setOnClickListener(this);
+        
+        this.setListAdapter( new ArrayAdapter<String>(this,android.R.layout.simple_list_item_checked, items));
+        
     }
     
     public void onClick(View v) {
+    	Intent it;
+    	switch (v.getId()) {
+		case R.id.btnInsert:
+    		it = new Intent("EVENTO_CAD_CONVIDADO");
+    		it.addCategory("BR_FATECSP");
+    		startActivity( it ); 
+			break;
 
+		case R.id.btnConfirmar:
+			//--------------------------------------
+			//  Incluir os registros marcados
+			// no banco, após isso recarregar
+			// a lista de convidados
+			//--------------------------------------
+			break;
+		default:
+			break;
+		}    	
+    }
+    
+    @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+    	//--------------------------------------
+    	//  Executar marcação e adição dos 
+    	// selecionados para lista de gravação 
+    	//--------------------------------------
+    	
+		CheckedTextView check = (CheckedTextView) v;
+		check.setChecked(!check.isChecked());
+    	
+	} 
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
     }
 }
