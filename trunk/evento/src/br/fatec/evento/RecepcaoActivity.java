f1a3fac7823d81305cf1b38e18dae5828d374433
shikamarujunior <shikamarujunior@ebf3cafe-77c0-995b-8895-a9585d18da41>
2013-01-15 12:37:12 +0000

    inclusão de modelo para leitura dos elementos da lista de recepção

--- trunk/evento/src/br/fatec/evento/RecepcaoActivity.java
+++ trunk/evento/src/br/fatec/evento/RecepcaoActivity.java
@@ -1,23 +1,35 @@
 package br.fatec.evento;
 
+import java.lang.reflect.Array;
+
 import br.fatec.evento.classes.Convidado;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 
 public class RecepcaoActivity extends ListActivity implements OnClickListener {
 	/** Called when the activity is first created. */
 	
-	String[] items = new String[] { "Convidado 1","Convidado 2","Convidado 3","Convidado 4", "Convidado 5", "Convidado 6"}; 
+	String[] eventos = new String[] { "Evento 1","Evento 2","Evento 3"};
+	String[] items1 = new String[] { "Ev 1 : Conv 1","Ev 1 : Conv 2","Ev 1 : Conv 3","Ev 1 : Conv 4", "Ev 1 : Conv 5", "Ev 1 : Conv 6"};
+	String[] items2 = new String[] { "Ev 1 : Conv 1","Ev 1 : Conv 2","Ev 1 : Conv 3","Ev 1 : Conv 4", "Ev 1 : Conv 5", "Ev 1 : Conv 6"};
+	String[] items3 = new String[] { "Ev 1 : Conv 1","Ev 1 : Conv 2","Ev 1 : Conv 3","Ev 1 : Conv 4", "Ev 1 : Conv 5", "Ev 1 : Conv 6"};
+
+	
+	
+	Spinner listaEventos;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,7 +45,37 @@ public class RecepcaoActivity extends ListActivity implements OnClickListener {
         Button btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
         btnConfirmar.setOnClickListener(this);
         
-        this.setListAdapter( new ArrayAdapter<String>(this,android.R.layout.simple_list_item_checked, items));
+        listaEventos = (Spinner) findViewById(R.id.boxEvento);
+        // -------------------------------------------
+        //  buscar a lista de eventos permitidos para visualização
+        // -------------------------------------------
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, eventos);
+        listaEventos.setAdapter( adapter );
+        listaEventos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView parent, View v, int posicao, long id) {
+				
+				switch (posicao) {
+				case 0:
+					refreshList(items1);
+					break;
+
+				case 1:
+					refreshList(items2);
+					break;
+				case 2:
+					refreshList(items3);
+					break;
+				default:
+					break;
+				}
+				
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView parent) {
+			}
+		});
         
     }
     
@@ -64,15 +106,23 @@ public class RecepcaoActivity extends ListActivity implements OnClickListener {
     	//  Executar marcação e adição dos 
     	// selecionados para lista de gravação 
     	//--------------------------------------
-    	
 		CheckedTextView check = (CheckedTextView) v;
-		check.setChecked(!check.isChecked());
-    	
+		boolean temp = !check.isChecked();
+		check.setChecked(temp);
+		
+		if ( temp )
+			
+		else
+			
 	} 
     
     @Override
     protected void onResume() {
     	// TODO Auto-generated method stub
     	super.onResume();
+    }
+    
+    protected void refreshList( String[] items ){
+		this.setListAdapter( new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items));
     }
 }
